f306ae9ffd72ccad443ffa884f58481fc3adf5f8
4455 <chitsanuphong.chomthong@gmail.com>
2025-08-21 20:29:46 +0700

    pull

--- Integrated_project_SC3_frontend/src/components/Common/Navbar.vue
+++ Integrated_project_SC3_frontend/src/components/Common/Navbar.vue
@@ -6,7 +6,7 @@ import Search from './Query/Search.vue';
 const handleSearch = (keyword) => {
   console.log("ค้นหา:", keyword)
 }
-
+const 
 </script>
 
 <template>
@@ -37,10 +37,12 @@ const handleSearch = (keyword) => {
 
     <!-- Icons -->
     <div class="flex flex-row gap-4">
+      <button @click="">
       <svg xmlns="http://www.w3.org/2000/svg" width="35px" height="35px" fill="currentColor" class="bi bi-person-fill"
         viewBox="0 0 16 16">
         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
       </svg>
+      </button>
       <svg xmlns="http://www.w3.org/2000/svg" height="35px" width="35px" viewBox="0 -960 960 960" fill="#000000">
         <path
           d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
