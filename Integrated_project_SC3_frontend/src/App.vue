9daa73db2c98fef70d67be01bab507d076d10f99
Nutchanon-TR <nutchanon.charo@mail.kmutt.ac.th>
2025-08-30 03:10:50 +0700

    hide navbar in verify page

--- Integrated_project_SC3_frontend/src/App.vue
+++ Integrated_project_SC3_frontend/src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
+import { useRoute } from 'vue-router'
+
 import navbar  from "./components/Common/Navbar.vue";
 import Toast from './components/Common/Toast.vue';
 
 // import { useAlertStore } from "@/stores/alertStore.js";
 // const alertStore = useAlertStore();
 // alertStore.addToast("The sale item has been updated.", "Success", "error");
+const route = useRoute()
+
+const hideNavbarOn = ['/login', '/verify-email']
+
 
 </script>
 <template>
-  <navbar />
+    <navbar v-if="!hideNavbarOn.includes(route.path)" />
     <Toast />
   <RouterView />
 </template>
